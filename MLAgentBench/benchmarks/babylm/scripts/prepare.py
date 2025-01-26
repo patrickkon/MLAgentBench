fab1159016cc4e3d255e975702cbf4dc70e74f8a
Ubuntu <ubuntu@ip-172-31-39-99.ec2.internal>
2025-01-26 15:42:28 +0000

    misc changes

--- MLAgentBench/benchmarks/babylm/scripts/prepare.py
+++ MLAgentBench/benchmarks/babylm/scripts/prepare.py
@@ -4,8 +4,10 @@ import pandas as pd
 taskname = "babylm"
 download_dir = "../env"
 
-subprocess.run(["wget", "https://github.com/babylm/babylm.github.io/raw/main/babylm_data.zip"], cwd=download_dir) 
-subprocess.run(["unzip", "-n", f"babylm_data.zip"], cwd=download_dir)
+subprocess.run(["wget", "-q", "https://github.com/babylm/babylm.github.io/raw/main/babylm_data.zip"], cwd=download_dir, stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL) 
+subprocess.run(["unzip", "-n", f"babylm_data.zip"], cwd=download_dir,  stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL)
 subprocess.run(["rm", f"babylm_data.zip"], cwd=download_dir)
 subprocess.run(["rm", "-rf", f"babylm_data/babylm_100M"], cwd=download_dir)
 subprocess.run(["rm", "-rf", f"__MACOSX"], cwd=download_dir)
